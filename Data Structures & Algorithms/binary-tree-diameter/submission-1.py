d8c2cf67596aab2132c6cb0d42e4484c57af208d
jojo_code_book <131355880+zoeyework@users.noreply.github.com>
2026-06-24 20:15:16 +0800

    Add: binary-tree-diameter - submission-1

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-1.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.max_diameter=0
+
+        def get_depth(node):
+            if not node:
+                return 0
+            left_depth=get_depth(node.left)
+            right_depth=get_depth(node.right)
+
+            self.max_diameter=max(self.max_diameter, left_depth+right_depth)
+            return max(left_depth, right_depth)+1
+        get_depth(root)
+        return self.max_diameter
+        
